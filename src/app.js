8c8b65e20ca136b3bd2a977962fa0499bec8e00f
GaneshRayala-Luvetha <ganesh.rayala@luvetha.com>
2026-03-12 14:32:39 +0530

    cors fixed

--- src/app.js
+++ src/app.js
@@ -8,26 +8,33 @@ const serviceRoutes = require("./routes/serviceRoutes");
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "http://localhost:3002",
-  "http://localhost:5000",
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "http://localhost:3001",
+//   "http://localhost:3002",
+//   "http://localhost:5173",
+//   "http://localhost:5000",
+// ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
+
+app.use(cors({
+    origin:'*',
+    methods:['GET','PUT','PATCH','DELETE','UPDATE','OPTIONS'],
+    allowedHeaders:['content-type','Authorization']
+}))
 
 app.use(express.json());
 
